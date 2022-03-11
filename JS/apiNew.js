a38d8524097e89c5804cf01f85043c10eff4ca83
sorinzubere <100420427+sorinzubere@users.noreply.github.com>
2022-03-11 15:50:05 +0200

    update style

--- JS/apiNew.js
+++ JS/apiNew.js
@@ -14,14 +14,14 @@ $(document).ready(function() {
      
        if(searchData === "" || searchData === null) {
          displayError();
-       }
-      else {
+       }else if(searchData === undefined){
+           alert("Something went wrong. Do another search.")
+       }else {
         
          $.ajax({
             url: bookUrl + searchData,
             dataType: "json",
             success: function(response) {
-              console.log(response)
               if (response.totalItems === 0) {
                 alert("no result!.. try again")
               }
@@ -36,7 +36,7 @@ $(document).ready(function() {
             }
           });
         }
-        $("#search-box").val(""); 
+        $("#search-box").val(searchData); 
      });
   
      function displayResults(response) {
@@ -46,7 +46,7 @@ $(document).ready(function() {
           author1 = item.volumeInfo.authors;
           publisher1 = item.volumeInfo.publisher;
           bookLink1 = item.volumeInfo.previewLink;
-          bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
+        
           bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
   
           item2 = response.items[i+1];
@@ -54,13 +54,13 @@ $(document).ready(function() {
           author2 = item2.volumeInfo.authors;
           publisher2 = item2.volumeInfo.publisher;
           bookLink2 = item2.volumeInfo.previewLink;
-          bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
+         
           bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
   
          
           outputList.innerHTML += '<div class="row mt-4">' +
-                                  formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
-                                  formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                                  formatOutput(bookImg1, title1, author1, publisher1, bookLink1) +
+                                  formatOutput(bookImg2, title2, author2, publisher2, bookLink2) +
                                   '</div>';
   
           console.log(outputList);
@@ -68,9 +68,9 @@ $(document).ready(function() {
      }
   
 
-     function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+     function formatOutput(bookImg, title, author, publisher, bookLink ) {
       
-       var viewUrl = 'book.html?isbn='+bookIsbn;
+       var viewUrl = 'book.html?title='+title;
        var htmlCard = `<div class="col-lg-6">
          <div class="card" style="">
            <div class="row no-gutters">
